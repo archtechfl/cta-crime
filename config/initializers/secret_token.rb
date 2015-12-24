73d46b592fd94fb2b555400bf54de707d9c94329
Jeremy Moore <jeremy@moremooredesign.com>
2015-12-24 14:01:54 -0500

    added missing files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CtaCrime::Application.config.secret_token = 'ec4576ef5575c5aeb59e6cc67a9a47de2c8406bf9867711a072daafaf0f57a7e6908b2f2e240a289fa372af8e7c8f245ae93f7eccc94c80e60aade66b6ae2336'
